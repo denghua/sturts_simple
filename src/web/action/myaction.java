b682b6674029133664310879d529e94acad9611e
unknown <denghua@aa.com>
2013-12-02 10:41:10 +0800

    myaction.java
     创建一个action类处理请求

---
+++ src/web/action/myaction.java
@@ -0,0 +1,12 @@
+package web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class myaction extends ActionSupport {
+
+	public String execute() throws Exception {
+		
+		return SUCCESS;
+	}
+
+}
