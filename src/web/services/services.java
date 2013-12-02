2c5e0269786dbf11c9cba8d4e5192b2acd303d22
unknown <denghua@aa.com>
2013-12-02 10:24:54 +0800

    新建文件services.java
    java bean(domain)(pojo)

---
+++ src/web/services/services.java
@@ -0,0 +1,21 @@
+package web.services;
+
+public class services {
+
+	private String uname;
+
+	public services() {
+
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+
+	
+}
